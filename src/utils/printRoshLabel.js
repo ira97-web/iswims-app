1c02ce6b6ae81a522ee0565c18e41fff13e32e63
Ira Wahadah <irawahadah@MacBook-Air-Ira.local>
2026-08-11 21:26:38 +0800

    Adjust PTj, ID SISA, and QR code overlay positioning on ROSH UKM label template

--- src/utils/printRoshLabel.js
+++ src/utils/printRoshLabel.js
@@ -68,7 +68,7 @@ export function handlePrintRoshWasteLabel(item, profile) {
           /* 1. PTJ NAME: POSITIONED RIGHT NEXT TO 'Nama PTj/ PTj Name:' */
           .field-ptj {
             top: 22px;
-            left: 650px;
+            left: 560px;
             font-size: 15px;
             text-align: left;
           }
@@ -76,7 +76,7 @@ export function handlePrintRoshWasteLabel(item, profile) {
           /* 2. ID SISA: CENTERED DIRECTLY BELOW 'Nama PTj/ PTj Name:' */
           .field-id-sisa {
             top: 58px;
-            left: 600px;
+            left: 380px;
             width: 280px;
             font-size: 12px;
             text-align: center;
